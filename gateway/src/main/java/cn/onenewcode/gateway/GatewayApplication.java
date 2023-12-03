7bf3a81ae62b550f58d130ebd25e31881e579f8a
29071 <lovestudy@qq.com>
2023-12-03 21:26:00 +0800

    geteway

---
+++ gateway/src/main/java/cn/onenewcode/gateway/GatewayApplication.java
@@ -0,0 +1,17 @@
+package cn.onenewcode.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 29071
+ * @version 1.0
+ * @data 2023/12/3 20:43
+ */
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
---
+++ gateway/src/main/java/cn/onenewcode/gateway/filters/GlobalFilter.java
@@ -0,0 +1,21 @@
+package cn.onenewcode.gateway.filters;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+/**
+ * @author 29071
+ * @version 1.0
+ * @data 2023/12/3 21:12
+ */
+public interface GlobalFilter {    /**
+ *  处理当前请求，有必要的话通过{@link GatewayFilterChain}将请求交给下一个过滤器处理
+ *
+ * @param exchange 请求上下文，里面可以获取Request、Response等信息
+ * @param chain 用来把请求委托给下一个过滤器
+ * @return {@code Mono<Void>} 返回标示当前过滤器业务结束
+ */
+    Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain);
+}
+
